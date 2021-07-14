2d02b354f2b0dcffa635d3d75c332349f655bc35
Aiat <isamidinovaiat@gmail.com>
2021-07-14 20:32:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var buttons = document.querySelectorAll(".drum");
+
+for (var i = 0; i < buttons.length; i++) {
+
+  buttons[i].addEventListener("click", function () {
+    alert("I got clicked");
+  })
+  
+}
